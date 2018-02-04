510e635590fcded1e8a7a5d27d3c5f9c0a6e5dec
Maciej <maciej.loziak@softylabs.com>
2018-02-04 22:59:14 +0100

    VM-29 Rename USER to MANAGER

--- ivms/src/main/kotlin/pl/loziuu/ivms/identity/user/domain/User.kt
+++ ivms/src/main/kotlin/pl/loziuu/ivms/identity/user/domain/User.kt
@@ -6,7 +6,7 @@ import javax.persistence.*
 @Table(name = "users")
 class User(@Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long = 0,
            @Enumerated(EnumType.STRING)
-           var role: Role = Role.USER,
+           var role: Role = Role.MANAGER,
            @Column(unique = true)
            val login: String = "",
            var password: String = "",
